6c6ce173f169713ac0dc611872754707c7d88eb2
Jin <jin.choi@cern.ch>
2020-11-26 08:36:34 +0100

    crab jobs

---
+++ crab_submit_files/crab_DYm50_1j_nlo_cuep5m1/crab_submit_DYm50_1j_nlo_cuep5m1.py
@@ -0,0 +1,23 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'DYm50_1j_nlo_cuep5m1'
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = '../../configs/DYm50_1j_nlo_cuep5m1_cfg.py'
+config.JobType.allowUndistributedCMSSW = True
+#config.JobType.numCores = 1
+
+config.Data.outputPrimaryDataset = 'DYm50_1j_nlo_cuep5m1'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10000 # num. of jobs to submit
+NJOBS = 800  # This is not a configuration parameter, but an auxiliary variable that we use in the next line.
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.outLFNDirBase = '/store/user/choij/' 
+config.Data.publication = True
+config.Data.outputDatasetTag = 'RunIISummer15wmLHEGS'
+
+config.Site.storageSite = 'T2_KR_KNU'
